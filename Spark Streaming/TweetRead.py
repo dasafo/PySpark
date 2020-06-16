3ffe223f28cdf864a3bc4ef83fdd95cf11169fce
dasafo <44670461+dasafo@users.noreply.github.com>
2020-06-16 20:19:25 +0200

    16-06-2020

--- Spark Streaming/TweetRead.py
+++ Spark Streaming/TweetRead.py
@@ -7,10 +7,10 @@ import json
 
 
 # Set up your credentials
-consumer_key=''
-consumer_secret=''
-access_token =''
-access_secret=''
+consumer_key='u89miYwyHmPu2XHdrgJqWPcBU'
+consumer_secret='J9uBls2k8nSUqiifl0xoFJDQ93Pf8cGH3G14kOwKK1dGgC9we2'
+access_token ='754714491875684352-ZXeKu9MNs4Xk8nyMI6sqvZl8LrbEku2'
+access_secret='elZhit57ybWPAnf8gswTcas30MzP97aCb2oWavel1Xtsa'
 
 
 class TweetsListener(StreamListener):
@@ -19,14 +19,15 @@ class TweetsListener(StreamListener):
       self.client_socket = csocket
 
   def on_data(self, data):
-      try:
-          msg = json.loads( data )
-          print( msg['text'].encode('utf-8') )
-          self.client_socket.send( msg['text'].encode('utf-8') )
-          return True
-      except BaseException as e:
-          print("Error on_data: %s" % str(e))
-      return True
+        try:
+            msg = json.loads( data ) # Create a message from json file
+            print( msg['text'].encode('utf-8') ) # Print the message and UTF-8 coding will eliminate emojis
+            ## self.client_socket.send( msg['text'].encode('utf-8') )  this line is wrong , add the "\n" 
+            self.client_socket.send((str(msg['text']) + "\n").encode('utf-8'))
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
 
   def on_error(self, status):
       print(status)
@@ -42,7 +43,7 @@ def sendData(c_socket):
 if __name__ == "__main__":
   s = socket.socket()         # Create a socket object
   host = "127.0.0.1"     # Get local machine name
-  port = 5555                 # Reserve a port for your service.
+  port = 9998                 # Reserve a port for your service.
   s.bind((host, port))        # Bind to the port
 
   print("Listening on port: %s" % str(port))
